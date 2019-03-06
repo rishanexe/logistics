7d88121af197721ec06160d2bf89b205e2ef2b5f
ri5han <ri5han@github.com>
2019-03-07 01:00:43 +0530

    register page created

--- register/views.py
+++ register/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'register/layout.html', {'form': form})
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('index')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'register/register.html', {'form': form})
